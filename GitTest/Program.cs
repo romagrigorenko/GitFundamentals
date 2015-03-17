6b5e01169acc7ce217c4376e187a85183478ef31
Orest AAAO <proficiencyconsulting.com>
2015-03-16 21:14:41 -0400

    add project

---
+++ GitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdfasdfasd");
+            Console.WriteLine("bug 0 Fixed ");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
--- readme.txt
+++
@@ -1,4 +0,0 @@
-Orest Is a nice guy
-
-Orest Is a very very nice guy
-OK OK
